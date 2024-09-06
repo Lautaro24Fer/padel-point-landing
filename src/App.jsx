a8652be14c98213e01d066d813aeb4850953bba4
TaiFilipelli <taielfilipelli@gmail.com>
2024-09-06 13:47:56 -0300

    Modificaciones en el logo, estilos de Tailwind instalados correctamente y primeros toques en la página

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { Fade, Slide } from "react-awesome-reveal"
+const MainPage = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section className="flex justify-center flex-col">
+      <Slide direction="left" delay={100} duration={1000} cascade>
+        <h1 className="font-bold text-4xl">Hola</h1>
+        <h2>Estamos trabajando en este sitio para vos</h2>
+      </Slide>
+      <Fade delay={1600} duration={1900}>
+        <p>Mientras tanto, seguimos en nuestras redes para no perderte ninguna novedad!</p>
+      </Fade>
+    </section>
   )
 }
 
-export default App
+export default MainPage
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import MainPage from './App'
+import '../styles/globals.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <main className='h-screen w-screen p-16 bg-[#264492] text-white'>
+      <MainPage />
+    </main>
   </StrictMode>,
 )
